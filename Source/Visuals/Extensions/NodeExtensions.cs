54da9b48f590ce52c900de600d4fb59a2bbf74c8
Marco Klein <marco.klein@trustedshops.de>
2023-05-23 00:45:09 +0200

    feat: :sparkles: Add GetContentsSize method to determine mesh sizes of or within a node.

--- Source/Visuals/Extensions/NodeExtensions.cs
+++ Source/Visuals/Extensions/NodeExtensions.cs
@@ -1,10 +1,30 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Godot;
 
 namespace GoDough.Visuals.Extensions {
   public static class NodeExtensions {
-    private static IEnumerable<Node> FindAllChildren(Node node,  Func<Node, bool> predicate, List<Node> nodeList) {
+    public static Vector3 GetContentsSize(this Node3D node, string groupName = "Mesh") {
+      var nodes = new List<MeshInstance3D>();
+      if (node is MeshInstance3D) {
+        nodes.Add(node as MeshInstance3D);
+      } else {
+        nodes = node
+          .FindAllChildren(x => x.IsInGroup(groupName))
+          .OfType<MeshInstance3D>()
+          .ToList();
+      }
+
+      var aabbs = nodes
+        .Select(x => x
+          .GetRotatedAndTransformedAabb())
+        .Aggregate((a, b) => a.Merge(b));
+
+      return aabbs.Abs().Size;
+    }
+
+    private static IEnumerable<Node> FindAllChildren(Node node, Func<Node, bool> predicate, List<Node> nodeList) {
       foreach(var child in node.GetChildren()) {
         if (predicate(child)) {
           nodeList.Add(child);
