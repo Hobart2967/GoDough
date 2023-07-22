7ac41f9af7477862073fbbad88fa56575b9d2c1b
Marco Klein <hobart2967@outlook.com>
2023-07-22 14:35:17 +0200

    feat: :sparkles: Add Resource Extensions to duplicate using a callback to modify during duplication

---
+++ Source/Visuals/Extensions/ControlExtensions.cs
@@ -0,0 +1,15 @@
+using System;
+using Godot;
+
+namespace GoDough.Visuals.Extensions {
+  public static class ResourceExtensions {
+    public static T DuplicateWith<T>(this Resource resource, Action<T> modifier)
+      where T : Resource {
+      var styleBox = resource.Duplicate() as T;
+      modifier(styleBox);
+      return styleBox;
+    }
+  }
+}
+
+
