ab81fc90d7ca446753f9a76cdc71b6c2a7cd15c8
Hobart2967 <hobart2967@outlook.com>
2024-08-23 23:26:05 +0200

    refactor: Rename stuff.

--- Source/Visuals/Extensions/Vector3Extensions.cs
+++ Source/Visuals/Extensions/Vector3Extensions.cs
@@ -1,23 +1,27 @@
 using System;
 using Godot;
 
-namespace GoDough.Visuals.Extensions {
-  public static class Vector3Extensions {
+namespace GoDough.Visuals.Extensions
+{
+  public static class Vector3Extensions
+  {
 
     public static Vector3 CopyWith(
       this Vector3 source,
-      Nullable<float> x,
+      Nullable<float> x = null,
       Nullable<float> y = null,
-      Nullable<float> z = null) {
+      Nullable<float> z = null)
+    {
       return new Vector3(
         x ?? source.X,
         y ?? source.Y,
         z ?? source.Z);
     }
 
-     public static Vector3 Copy(
-      this Vector3 source) {
-      return source.CopyWith(null);
+    public static Vector3 Copy(
+     this Vector3 source)
+    {
+      return source.CopyWith();
     }
   }
 }
