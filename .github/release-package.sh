f754f1f9e89b6611bc785e426e48580eaf31bd08
Hobart2967 <hobart2967@outlook.com>
2024-08-23 23:29:05 +0200

    ci: Fix missing decommentation of publishing

--- .github/release-package.sh
+++ .github/release-package.sh
@@ -1,4 +1,4 @@
 VERSION=$1
 
 dotnet pack --configuration Release
-echo dotnet nuget push GoDough.${VERSION}.nupkg --api-key $NUGET_API_KEY
+dotnet nuget push GoDough.${VERSION}.nupkg --api-key $NUGET_API_KEY
