a5cd9916687889fcfa4de47550f7dc808eff3ebb
Hobart2967 <hobart2967@outlook.com>
2024-08-23 23:40:31 +0200

    ci: Fix package source path

--- .github/release-package.sh
+++ .github/release-package.sh
@@ -2,6 +2,6 @@ VERSION=$1
 
 dotnet pack --configuration Release
 dotnet nuget push \
-  GoDough.${VERSION}.nupkg \
+  .godot/mono/temp/bin/Release/GoDough.${VERSION}.nupkg \
   --api-key $NUGET_API_KEY \
   --source https://api.nuget.org/v3/index.json
